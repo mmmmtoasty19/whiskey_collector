af600e80c2699f2fd98bd61aae1afa32a7721376
Kyle Belanger <Kyleb44@hotmail.com>
2025-03-06 15:19:15 -0500

    add tailwind css

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    tailwindcss()
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
