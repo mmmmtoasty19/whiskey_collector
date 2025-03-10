69f0658d4baa601417f75007d52073d13dc89924
Kyle Belanger <Kyleb44@hotmail.com>
2025-03-10 14:28:29 -0400

    switching to react

--- frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import './app.css';
-import App from './App.svelte';
-
-// Svelte 5 syntax for mounting a component
-const app = App({
-  target: document.getElementById('app')
-});
-
-export default app;
